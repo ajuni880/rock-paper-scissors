5db0f172207e2a7f9f7552efaad114d284b45803
Junaid Aslam <ajunaid880@gmail.com>
2020-10-03 15:12:03 +0200

    remove unused code

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,6 +1,2 @@
 export { default as BaseIconWrapper } from './BaseSvgIconWrapper';
 export * from './Close';
-export * from './Paper';
-export * from './Rock';
-export * from './Scissors';
-export * from './Rules';
